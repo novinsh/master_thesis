025f5c8a852e528c1aa4b1979e3dc932eff82c26
Novin Shahroudi <novinsha@gmail.com>
2020-06-23 12:30:52 +0300

    Some updates to the baseline naive model and a new version of it.

--- models/baselines/quantile_naive.py
+++ models/baselines/quantile_naive.py
@@ -11,22 +11,42 @@ class QuantileNaive(ModelBase):
         self.probabilisticForecast = None
         self.warmupForecast = forecast_with_warmup
 
-    def fit(self, X, y, **argv):
+    def fit(self, train, dev, **argv):
         self.probabilisticForecast = None
         if not self.warmupForecast:
-            ypred_naive = naive_quantile(X, self.horizon)
+            ypred_naive = naive_quantile(train, self.horizon)
             self.probabilisticForecast = ProbabilisticForecast(ypred_naive.flatten().tolist())
 
-    def forecast(self, warmup, test, **argv):
+    def forecast(self, warmup, test=None, **argv):
         if self.warmupForecast:
             ypred_naive = naive_quantile(warmup, self.horizon)
             self.probabilisticForecast = ProbabilisticForecast(ypred_naive.flatten().tolist())
         ypred_median = self.probabilisticForecast.median()
         return ypred_median
 
-    def forecast_probabilistic(self, warmup, test, **argv):
+    def forecast_probabilistic(self, warmup, test=None, **argv):
         if self.warmupForecast:
             ypred_naive = naive_quantile(warmup, self.horizon)
             self.probabilisticForecast = ProbabilisticForecast(ypred_naive.flatten().tolist())
         return self.probabilisticForecast
-    
+
+
+class QuantileNaiveNew(ModelBase):
+    """ Simplified verison of the above class, make use of this everywhere! """
+    def __init__(self):
+        super(ModelBase, self).__init__()
+        self.probabilisticForecast = None
+        self.train_data = None
+
+    def fit(self, train, dev=None, **argv):
+        self.train_data = train
+
+    def forecast(self, warmup, test, **argv):
+        ypred_naive = naive_quantile(self.train_data, horizon=len(test))
+        self.probabilisticForecast = ProbabilisticForecast(ypred_naive.flatten().tolist())
+        return self.probabilisticForecast.median()
+
+    def forecast_probabilistic(self, warmup, test=None, **argv):
+        ypred_naive = naive_quantile(self.train_data, horizon=len(test))
+        self.probabilisticForecast = ProbabilisticForecast(ypred_naive.flatten().tolist())
+        return self.probabilisticForecast
